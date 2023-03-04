8426ed18263b9bdd2044422fbceea3a3d4bcf9d4
AndyLin-1 <www.andy@hotmail.ca>
2023-03-04 13:44:27 -0500

    Initalized Project

---
+++ CrystalOptical-backend/crystaloptical/src/main/java/com/example/crystaloptical/CrystalopticalApplication.java
@@ -0,0 +1,13 @@
+package com.example.crystaloptical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrystalopticalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrystalopticalApplication.class, args);
+    }
+
+}
---
+++ CrystalOptical-backend/crystaloptical/src/test/java/com/example/crystaloptical/CrystalopticalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crystaloptical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrystalopticalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
