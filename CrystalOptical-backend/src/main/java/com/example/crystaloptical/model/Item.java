e7c25ffffe117a2acc78be4d20fc38253c4bd4f1
Patrick <li.patrick107@gmail.com>
2023-03-12 16:37:51 -0400

    Add the Item class and the Item Repository

---
+++ CrystalOptical-backend/src/main/java/com/example/crystaloptical/model/Item.java
@@ -0,0 +1,97 @@
+package com.example.crystaloptical.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class Item {
+    @Id
+    private Long id;
+
+    /**
+     * Attributes based on multiple glasses websites
+     * Example: https://www.clearly.ca/eyewear
+     */
+
+    private String glassesName;
+
+    private double price;
+
+    /**
+     * Brand name of glasses
+     * Examples:
+     * 1. Clearly Basic
+     * 2. Joseph Marc
+     * 3. Love
+     * 4. Ray-Ban
+     */
+    private String brand;
+
+
+    /**
+     * Lens Material:
+     * 1. Metal
+     * 2. Stainless Steel
+     * 3. Plastic
+     */
+    private String lensMaterial;
+
+    /**
+     * frameWidth:
+     * 1. Small
+     * 2. Medium
+     * 3. Large
+     */
+    private String frameSize;
+
+    /**
+     * Examples:
+     * Rectangle
+     * Square
+     * Round
+     */
+    private String frameShape;
+
+    /**
+     * Examples:
+     * 1. Full Rim
+     * 2. Rimless
+     * 3. Semi Rimless
+     */
+    private String rimType;
+
+
+    private String colour;
+
+    /**
+     * Could delete but a lot of size have them.
+     * Examples:
+     * Men
+     * Women
+     */
+    private String gender;
+
+    private double rating;
+
+
+    public Item(){}
+
+    public Item(String glassesName, double price, String brand, String frameSize, String frameShape, String rimType, String lensMaterial,
+                String colour, String gender, double rating){
+        this.glassesName = glassesName;
+        this.price = price;
+        this.brand = brand;
+        this.frameSize = frameSize;
+        this.frameShape = frameShape;
+        this.rimType = rimType;
+        this.lensMaterial = lensMaterial;
+        this.colour = colour;
+        this.gender = gender;
+        this.rating = rating;
+
+    }
+}
