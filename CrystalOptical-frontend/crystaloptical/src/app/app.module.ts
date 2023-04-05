b9dc33da832804fcf76f13912743b7bd84713bab
Patrick <li.patrick107@gmail.com>
2023-04-05 01:10:41 -0400

    Included the header, finished the creating account, working on log-in

--- CrystalOptical-frontend/crystaloptical/src/app/app.module.ts
+++ CrystalOptical-frontend/crystaloptical/src/app/app.module.ts
@@ -8,15 +8,18 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import { SimpleRegisterComponent } from './modules/simple-register/simple-register.component';
 import { SimpleItemComponent } from './modules/simple-item/simple-item.component';
-import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppHeaderComponent } from './modules/app-header/app-header.component';
+import { AppFooterComponent } from './modules/app-footer/app-footer.component';
 import { ItemDataComponent } from './item-data/item-data.component';
 import { RelatedItemComponent } from './related-item/related-item.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import { CreateAccountComponent } from './modules/create-account/create-account.component';
+import { LogInComponent } from './modules/log-in/log-in.component';
 
 
 @NgModule({
-  declarations: [AppComponent, SimpleInterfaceComponent, SimpleRegisterComponent, SimpleItemComponent, AppHeaderComponent, ItemDataComponent, RelatedItemComponent],
+  declarations: [AppComponent, SimpleInterfaceComponent, SimpleRegisterComponent, SimpleItemComponent, AppHeaderComponent, AppFooterComponent,ItemDataComponent, RelatedItemComponent, CreateAccountComponent, LogInComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
