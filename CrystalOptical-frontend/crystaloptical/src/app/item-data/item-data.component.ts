6382979c1223bfa4c6029158be1c434cddb30e5e
Armani Araujo <aaraujo3@my.yorku.ca>
2023-04-04 20:26:15 -0400

    Item data and related item components added

---
+++ CrystalOptical-frontend/crystaloptical/src/app/item-data/item-data.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'item-data',
+  templateUrl: './item-data.component.html',
+  styleUrls: ['./item-data.component.css']
+})
+export class ItemDataComponent implements OnInit {
+  glassesTitle:string = 'Glasses Test Title';
+  numStars:number = 4.2;
+  numReviews:number = 4200;
+  inStock:boolean = true;
+  price:number = 420.60;
+  description:string =  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nisi dolor, ' +
+    'varius quis tortor sed, cursus congue nulla. Maecenas a diam posuere, fringilla erat at, iaculis ' +
+    'turpis. Fusce aliquam turpis vel augue hendrerit consectetur. Nulla a ligula malesuada, hendrerit ';
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
