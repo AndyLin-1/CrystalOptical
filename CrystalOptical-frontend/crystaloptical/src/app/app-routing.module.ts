eac2d6e0c72bde61a27b2d349dae6ec2ea286b75
Patrick <li.patrick107@gmail.com>
2023-04-07 00:02:13 -0400

    Updated so when running ng serve, the components of header and footer fit.

--- CrystalOptical-frontend/crystaloptical/src/app/app-routing.module.ts
+++ CrystalOptical-frontend/crystaloptical/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
+import {ItemDataComponent} from "./item-data/item-data.component";
+import {LogInComponent} from "./modules/log-in/log-in.component";
+import {CreateAccountComponent} from "./modules/create-account/create-account.component";
 
 const routes: Routes = [
+  {path: "login", component: LogInComponent},
+  {path: "register", component: CreateAccountComponent},
+  {path: "itemsExample", component: ItemDataComponent},
   {path: "**", redirectTo: "/"}];
 
 
