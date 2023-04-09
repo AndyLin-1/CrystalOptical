46d08c6b18ef9319a92e34dd807c90f9f7ce29ef
Patrick <li.patrick107@gmail.com>
2023-04-08 23:57:37 -0400

    Added Chatbot Toggle and basic Chatbot window

--- CrystalOptical-frontend/src/app/modules/chatbot/chatbot.component.ts
+++ CrystalOptical-frontend/src/app/modules/chatbot/chatbot.component.ts
@@ -10,6 +10,110 @@ export class ChatbotComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // MESSAGE INPUT
+    const textarea = document.querySelector('.chatbox-message-input') as HTMLTextAreaElement;
+    const chatboxForm = document.querySelector('.chatbox-message-form') as HTMLFormElement;
+
+    textarea.addEventListener('input', function () {
+      let line = textarea.value.split('\n').length;
+
+      if(textarea.rows < 6 || line < 6) {
+        textarea.rows = line;
+      }
+
+      if(textarea.rows > 1) {
+        chatboxForm.style.alignItems = 'flex-end';
+      } else {
+        chatboxForm.style.alignItems = 'center';
+      }
+    });
+
+
+    // TOGGLE CHATBOX
+    const chatboxToggle = document.querySelector('.chatbox-toggle') as HTMLElement;
+    const chatboxMessage = document.querySelector('.chatbox-message-wrapper') as HTMLElement;
+
+    chatboxToggle.addEventListener('click', function () {
+      chatboxMessage.classList.toggle('show');
+    });
+
+
+  // DROPDOWN TOGGLE
+    const dropdownToggle = document.querySelector('.chatbox-message-dropdown-toggle') as HTMLElement;
+    const dropdownMenu = document.querySelector('.chatbox-message-dropdown-menu') as HTMLElement;
+
+    dropdownToggle.addEventListener('click', function () {
+      dropdownMenu.classList.toggle('show');
+    });
+
+    document.addEventListener('click', function (e) {
+      if (e.target && !(e.target as HTMLElement).matches('.chatbox-message-dropdown, .chatbox-message-dropdown *')) {
+        dropdownMenu.classList.remove('show');
+      }
+    });
+
+// CHATBOX MESSAGE
+    const chatboxMessageWrapper = document.querySelector('.chatbox-message-content') as HTMLElement;
+    const chatboxNoMessage = document.querySelector('.chatbox-message-no-message') as HTMLElement;
+
+    chatboxForm.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      if(isValid(textarea.value)) {
+        writeMessage();
+        setTimeout(autoReply, 1000);
+      }
+    });
+
+
+
+    function addZero(num: number): string {
+      return num < 10 ? '0'+num : num.toString();
+    }
+
+    function writeMessage() {
+      const today = new Date();
+      let message = `
+    <div class="chatbox-message-item sent">
+      <span class="chatbox-message-item-text">
+        ${textarea.value.trim().replace(/\n/g, '<br>\n')}
+      </span>
+      <span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(today.getMinutes())}</span>
+    </div>
+  `;
+      chatboxMessageWrapper.insertAdjacentHTML('beforeend', message);
+      chatboxForm.style.alignItems = 'center';
+      textarea.rows = 1;
+      textarea.focus();
+      textarea.value = '';
+      chatboxNoMessage.style.display = 'none';
+      scrollBottom();
+    }
+
+    function autoReply() {
+      const today = new Date();
+      let message = `
+    <div class="chatbox-message-item received">
+      <span class="chatbox-message-item-text">
+        Thank you for your awesome support!
+      </span>
+      <span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(today.getMinutes())}</span>
+    </div>
+  `;
+      chatboxMessageWrapper.insertAdjacentHTML('beforeend', message);
+      scrollBottom();
+    }
+
+    function scrollBottom() {
+      chatboxMessageWrapper.scrollTo(0, chatboxMessageWrapper.scrollHeight);
+    }
+
+    function isValid(value: string): boolean {
+      let text = value.replace(/\n/g, '');
+      text = text.replace(/\s/g, '');
+
+      return text.length > 0;
+    }
   }
 
 }
