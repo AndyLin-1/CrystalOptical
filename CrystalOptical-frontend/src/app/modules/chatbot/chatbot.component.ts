eb83bf43157380586465376011d3c4b3692a4b3b
Patrick <li.patrick107@gmail.com>
2023-04-09 01:44:46 -0400

    Added Chatbot Toggle and basic Chatbot window

--- CrystalOptical-frontend/src/app/modules/chatbot/chatbot.component.ts
+++ CrystalOptical-frontend/src/app/modules/chatbot/chatbot.component.ts
@@ -10,6 +10,7 @@ export class ChatbotComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
     // MESSAGE INPUT
     const textarea = document.querySelector('.chatbox-message-input') as HTMLTextAreaElement;
     const chatboxForm = document.querySelector('.chatbox-message-form') as HTMLFormElement;
@@ -56,6 +57,7 @@ export class ChatbotComponent implements OnInit {
     const chatboxMessageWrapper = document.querySelector('.chatbox-message-content') as HTMLElement;
     const chatboxNoMessage = document.querySelector('.chatbox-message-no-message') as HTMLElement;
 
+
     chatboxForm.addEventListener('submit', function (e) {
       e.preventDefault();
 
@@ -69,6 +71,7 @@ export class ChatbotComponent implements OnInit {
       return num < 10 ? '0'+num : num.toString();
     }
 
+    /**
     function writeMessage() {
       const today = new Date();
       let message = `
@@ -86,6 +89,58 @@ export class ChatbotComponent implements OnInit {
       textarea.value = '';
       chatboxNoMessage.style.display = 'none';
       scrollBottom();
+    }*/
+
+    const utterances = [
+      ["how are you", "how is life", "how are things"], //0
+      ["hi", "hey", "hello", "good morning", "good afternoon"], //1
+      ["what are you doing", "what is going on", "what is up"], //2
+      ["how old are you"], //3
+      ["who are you", "are you human", "are you bot", "are you human or bot"], //4
+      // Add more utterances here as needed
+    ];
+
+    const answers = [
+      ["Fine... how are you?",    "Pretty well, how are you?",    "Fantastic, how are you?"], //0
+      [    "Hello!",    "Hi!",    "Hey!",    "Hi there!",    "Howdy",  ], //1
+      [    "Nothing much",    "About to go to sleep",    "Can you guess?",    "I don't know actually",  ], //2
+      ["I am infinite"], //3
+      ["I am just a bot", "I am a bot. What are you?"], //4
+      // Add more responses here as needed
+    ];
+
+    const alternatives = ["Go on...", "Try again"];
+
+
+    function writeMessage() {
+      const today = new Date();
+      let text = textarea.value.trim().replace(/[^\w\s\d]/gi, "");
+      output(text);
+      let message = `
+          <div class="chatbox-message-item sent" style=" align-self: flex-end; background: blue; color: white; border-radius: .75rem 0 .75rem .75rem; padding-left: 90%">
+            <div class="chatbox-message-item-text">
+              ${text}
+            </div>
+            <div class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(today.getMinutes())}</div>
+          </div>
+        `;
+      chatboxMessageWrapper.insertAdjacentHTML('beforeend', message);
+      chatboxForm.style.alignItems = 'center';
+      textarea.rows = 1;
+      textarea.focus();
+      textarea.value = '';
+      chatboxNoMessage.style.display = 'none';
+      scrollBottom();
+    }
+
+    function output(input: string): string {
+      let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+      text = text
+        .replace(/ a /g, " ")
+        .replace(/whats/g, "what is")
+        .replace(/please /g, "")
+        .replace(/ please/g, "");
+      return text;
     }
 
     function autoReply() {
